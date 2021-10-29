02dee3bdfa6ee64a87c65735049141abde6da20d
automationgeek <jf.tremblay@gmail.com>
2021-10-29 11:34:00 -0400

    Update milight_poly.py

--- milight_poly.py
+++ milight_poly.py
@@ -142,7 +142,7 @@ class MiLightLight(udi_interface.Node):
         controller.subscribe(controller.START, self.start, address)
 
     def start(self):
-        LOGGER.info('Test ' + self.name + self.milight_port )
+        LOGGER.info('Test ' + self.milight_host + str(self.milight_port) )
         self.__ConnectWifiBridge()
         self.setDriver('ST', 0, True)
         self.setDriver('GV1', 0, True)
@@ -291,7 +291,7 @@ class MiLightBridge(udi_interface.Node):
         controller.subscribe(controller.START, self.start, address)
 
     def start(self):
-        LOGGER.info('Test ' + self.name + self.milight_port )
+        LOGGER.info('Test ' + self.milight_host + str(self.milight_port) )
         self.__ConnectWifiBridge()
 
         # Init Value
@@ -371,7 +371,7 @@ class MiLightBridge(udi_interface.Node):
                 LOGGER.warning('Unable to setWhiteModeBridgeLamp')
 
     def __ConnectWifiBridge(self):
-        LOGGER.info('Test ' + self.name + self.milight_port )
+        LOGGER.info('Test ' + self.milight_host + str(self.milight_port) )
         if ( self.myMilight.setup(self.milight_host,self.milight_port,self.milight_timeout) == False ):
             LOGGER.error('Unable to setup MiLight')
 
